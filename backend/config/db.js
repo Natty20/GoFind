ae498667725ad89167131bf0277efbb0f4e61b40
root <root@vps-c76229be.vps.ovh.net>
2025-04-01 00:37:54 +0000

    Modifications faites sur le serveur

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://mongo:27018/GF');
-;
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('MongoDB connected yessirrr...');
     } catch (err) {
-        console.error(err.message);
+        console.error("MongoDB connection error : ", err.message);
         process.exit(1); // Arrêt du processus si la connexion échoue
     }
 };
