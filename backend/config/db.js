1254a7574f6227936d47ba6cc54cd8b60302855f
Natty20 <giginatty20@gmail.com>
2025-07-28 23:14:19 +0200

    feat: fix mongo atlas connection paths + updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
 
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://mongo:27018/GF");
+//     console.log("MongoDB connected yessirrr...");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1); // Arrêt du processus si la connexion échoue
+//   }
+// };
+
+const uri = process.env.MONGO_URI || 'mongodb+srv://gihozo:GoFind2025@gofind.mqur3gf.mongodb.net/GF';
+
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://mongo:27018/GF");
+    await mongoose.connect(uri);
     console.log("MongoDB connected yessirrr...");
   } catch (err) {
     console.error(err.message);
