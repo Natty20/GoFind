f7776d3e9f3295d8d0f985212a0ac1736fd1d0e0
Natty20 <giginatty20@gmail.com>
2025-12-18 15:58:56 +0100

    j'ai ajouter certaines pages pour pouvoir gérer les images lors d'une inscription

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,29 +1,24 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../middlewares/uploads");
-const cloudinary = require("../config/cloudinary");
-
 router.post("/image", upload.single("image"), async (req, res) => {
     try {
         if (!req.file) {
             return res.status(400).json({ message: "Aucune image envoyée" });
         }
 
-        const result = await cloudinary.uploader.upload_stream(
+        const stream = cloudinary.uploader.upload_stream(
             { folder: "gofind" },
             (error, result) => {
                 if (error) {
+                    console.error("Cloudinary error:", error);
                     return res.status(500).json({ error: error.message });
                 }
-                return res.json({ url: result.secure_url });
+
+                return res.status(200).json({ url: result.secure_url });
             }
         );
 
-        result.end(req.file.buffer);
+        stream.end(req.file.buffer);
     } catch (err) {
-        console.error(err);
+        console.error("Upload error:", err);
         res.status(500).json({ message: "Erreur upload image" });
     }
 });
-
-module.exports = router;
